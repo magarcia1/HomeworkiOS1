d7ca9534b05517954b472319cfb3eb81bfc77663
Miguel Garcia <miguelgarcia@Miguels-MacBook-Pro.local>
2016-11-22 13:46:32 -0700

    First Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Miguel Garcia on 11/14/16.
+//  Copyright © 2016 GCC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private var brain: CalculatorBrain = CalculatorBrain()
+    private var userIsInTheMiddleOfTyping: Bool = false
+
+    @IBOutlet private weak var display: UILabel!
+    @IBOutlet weak var displayDescription: UILabel!
+    
+    @IBAction private func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle! //1
+        if (userIsInTheMiddleOfTyping && digit != ".") ||
+            (digit == "." && (display.text!.range(of: ".") == nil)){
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            if digit == "." {
+                display.text =  "0" + digit
+            } else {
+                display.text = digit
+            }
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+    
+    private var displayValue : Double {
+        get {
+            //! optional: we have to account for every string passed
+            return Double(display.text!)!
+        }
+        set {
+            //Double can always be converted to a string
+            display.text = String(newValue)
+        }
+    }
+    
+    @IBAction private func performOperation(_ sender: UIButton){
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(operand: displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let matematicalSymbol = sender.currentTitle {
+            brain.performOperation(symbol: matematicalSymbol)
+        }
+        displayValue = brain.result
+    }
+    
+    var saveProgram: CalculatorBrain.PropertyList?
+    /*
+    @IBAction func save() {
+        saveProgram = brain.program
+    }
+    
+    @IBAction func restore() {
+        if saveProgram != nil {
+            brain.program = saveProgram!
+            displayValue = brain.result
+        }
+    }
+    */
+    @IBAction func clearButton() {
+        userIsInTheMiddleOfTyping = false
+        brain.clear()
+        displayValue = brain.result
+    }
+}
+//1 Optionals are allways initiallize to nil
+//1 Current title is an optional. Its associated value is a string
+//2 Print("Touched key \(digit)") for debuggin purposes
+//3 We do not have to unwrap to set the value of a variable.
+//  Specifictly, we are saying set this optional to set state with
+//  an associate value of textCurrentlyInDisplay + digit
+//4 Display is always set after the first nanosecond of activating the app.
+//  This is the reason why we unwrap the optional at the begining of the exectution
+
---
+++ CalculatorBrain.swift
@@ -0,0 +1,130 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Miguel Garcia on 11/14/16.
+//  Copyright © 2016 GCC. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain{
+    
+    private var accumulator: Double = 0.0
+    
+    var result: Double{ get{ return accumulator } }
+    
+    var isPartialResult: Bool = false
+    
+    var description: String?
+    
+    enum Operation{
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case Equals
+    }
+    
+    private var operations: Dictionary<String,Operation> = [
+        "π" : .Constant(M_PI),
+        "e" : .Constant(M_E),
+        "±" : .UnaryOperation({ -$0}),
+        "√" : .UnaryOperation(sqrt),
+        "cos" : .UnaryOperation(cos),
+        "sin" : .UnaryOperation(sin),
+        "tan" : .UnaryOperation(tan),
+        "log" : .UnaryOperation(log),
+        "+" : .BinaryOperation({$0 + $1}),
+        "−" : .BinaryOperation({$0 - $1}),
+        "×" : .BinaryOperation({$0 * $1}),
+        "÷" : .BinaryOperation({$1 / $0}),
+        "=" : .Equals
+    ]
+    
+    struct PendingBinaryOperationInfo {     //1
+        var firstOperand: Double
+        var binaryFunction: (Double, Double) -> Double
+    }
+    
+    private var pending: PendingBinaryOperationInfo?    //2
+    
+    // Intended to be Double if it is an operand
+    // String if it is an operations
+    private var internalProgram = [AnyObject]()
+    
+    func performOperation(symbol: String){
+        internalProgram.append(symbol as AnyObject)
+        if let operation = operations[symbol]{
+            switch operation {
+            case .Constant(let value):
+                accumulator = value
+            case .UnaryOperation(let function):
+                accumulator = function(accumulator)
+            case .BinaryOperation(let function):
+                isPartialResult = true
+                executePendingbinaryOperation()
+                pending = PendingBinaryOperationInfo(firstOperand: accumulator,binaryFunction: function)
+            case .Equals:
+                executePendingbinaryOperation()
+            }
+        }
+    }
+    
+    func executePendingbinaryOperation(){
+        if pending != nil {
+            //binaryFunction knows what to perform since
+            //we specified the operation in case 3
+            accumulator = pending!.binaryFunction(accumulator, pending!.firstOperand)
+            pending = nil
+            isPartialResult = false
+        }
+    }
+    
+    func setOperand(operand: Double){
+        accumulator = operand
+        internalProgram.append(operand as AnyObject)
+    }
+    
+    var variableValues: Dictionary<String, Double> = [:]
+    
+    func setOperator(operand: String){
+        variableValues[operand] = 0.0
+    }
+    
+    typealias PropertyList = AnyObject
+    
+    var program: PropertyList{
+        get{
+            return internalProgram as CalculatorBrain.PropertyList
+        }
+        set{
+            clear()
+            if let arrayofOps = newValue as? [PropertyList] {
+                for op in arrayofOps {
+                    if let operand = op as? Double {
+                        setOperand(operand: operand)
+                    }
+                    else if let operand = op as? String {
+                        performOperation(symbol: operand)
+                    }
+                }
+            }
+        }
+    }
+    
+    func clear(){
+        accumulator = 0.0
+        pending = nil
+        internalProgram.removeAll()
+    }
+}
+
+//1 An array is an struct.
+//2 Nil if not referenced or not holding a value
+
+//Clousure: Inline function that captures the state of its environment
+//"+" : Operation.BinaryOperation({(op1: Double, op2: Double) -> Double in return op1 + op2 } )
+//"+" : Operation.BinaryOperation({(op1, op2) in return op1 + op2 } )
+//"+" : Operation.BinaryOperation({($0, $1) in return $0 + $1 } )
+//"+" : Operation.BinaryOperation({$0 + $1}) //type inference
+
